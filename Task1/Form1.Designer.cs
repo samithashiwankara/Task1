177bbdd004259a9bf63925b6172a8d0d960ba933
Samitha Shiwankara <samithashiwankara46@gmail.com>
2024-02-17 23:31:40 +0530

    Intial commit

---
+++ Task1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Task1
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.txt_userName = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_login.Location = new System.Drawing.Point(97, 173);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(120, 31);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "LOGIN";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_clear.Location = new System.Drawing.Point(241, 173);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(109, 31);
+            this.btn_clear.TabIndex = 1;
+            this.btn_clear.Text = "Clear ";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // txt_userName
+            // 
+            this.txt_userName.Location = new System.Drawing.Point(165, 65);
+            this.txt_userName.Name = "txt_userName";
+            this.txt_userName.Size = new System.Drawing.Size(160, 23);
+            this.txt_userName.TabIndex = 2;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(165, 113);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(160, 23);
+            this.txt_password.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(40, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "UserName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(40, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(127, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(127, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = ":";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 277);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_userName);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.TextBox txt_userName;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Task1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Task1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\\Users\Samitha Shiwankara\OneDrive\Documentslogin_new.mdf;Integrated Security=True;Connect Timeout=30");
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            String username, uesr_password;
+            username = txt_userName.Text;
+            uesr_password = txt_password.Text;
+
+            try
+            {
+                String querry = "SELECT COUNT (*) FROM login WHERE user_name ='" + txt_userName.Text + "' AND passowrd ='" + txt_password.Text + "' ";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, con);
+
+                DataTable dtable = new System.Data.DataTable();
+                sda.Fill(dtable);
+
+                if(dtable.Rows.Count == 1)
+                {
+                    username = txt_userName.Text;
+                    uesr_password = txt_password.Text;
+
+                    //page that needed to be load next 
+                    this.Hide();
+
+                     MDIParent1 ss = new MDIParent1();
+                     ss.Show();
+                      
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login details");
+                    txt_userName.Clear();
+                    txt_password.Clear();
+
+                    //to foucs Username
+                    txt_userName.Focus();
+                }
+                    
+                   
+             }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                con.Close();
+            }
+         }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_userName.Clear();
+            txt_password.Clear();
+
+            txt_userName.Focus();
+        }
+    }
+}
